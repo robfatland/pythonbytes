ad0aeab86476ec1791051904f191b28e8ae18729
Rob Fatland <rob5@uw.edu>
2019-02-10 10:17:28 -0800

    adsf

--- projects/bees/example.py
+++ projects/bees/example.py
@@ -1,10 +1,8 @@
 # This section of code: Keep as-is
-import requests
-import numpy as np
-import matplotlib.pyplot as plt
-def bees(x, y, z): 
-  return requests.get('https://52t7suregg.execute-api.us-east-1.amazonaws.com/' + \
+import requests, numpy as np, matplotlib.pyplot as plt
+def bees(x, y, z): return requests.get('https://52t7suregg.execute-api.us-east-1.amazonaws.com/' + \
         'default/dronebees?' + 'x=' + str(x) + '&y=' + str(y) + '&z=' + str(z)).text
+
 
 # Test code. Note: This will occasionally give you 'drone lost' as the result
 # print(bees(2000., 2000., 50.))
@@ -14,8 +12,8 @@ def bees(x, y, z):
 # This is where you modify the code; check with a coach for more details
 nStops = 5
 dronecounts = np.zeros((nStops,nStops)) 
-result = bees(x, y, z)
-dronecounts[i, j] = float(result)
+# result = bees(x, y, z)
+# dronecounts[i, j] = float(result)
 
 
 
