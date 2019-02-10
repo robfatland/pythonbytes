a5d74d2be4056db73cac1a7d84d6fd20d84905d8
Rob Fatland <rob5@uw.edu>
2019-02-10 10:15:27 -0800

    ex

---
+++ projects/bees/example.py
@@ -0,0 +1,24 @@
+# This section of code: Keep as-is
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+def bees(x, y, z): 
+  return requests.get('https://52t7suregg.execute-api.us-east-1.amazonaws.com/' + \
+        'default/dronebees?' + 'x=' + str(x) + '&y=' + str(y) + '&z=' + str(z)).text
+
+# Test code. Note: This will occasionally give you 'drone lost' as the result
+# print(bees(2000., 2000., 50.))
+
+
+
+# This is where you modify the code; check with a coach for more details
+nStops = 5
+dronecounts = np.zeros((nStops,nStops)) 
+result = bees(x, y, z)
+dronecounts[i, j] = float(result)
+
+
+
+# This section of code: Keep as-is
+fig = plt.figure(figsize=(6, 3.2)); ax = fig.add_subplot(111); ax.set_title('My Baobab Orchard')
+plt.imshow(dronecounts); fig.savefig('graph.png')
